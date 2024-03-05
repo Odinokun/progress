19a037eb7d871e47e0062157c83190f188e4b3bc
odinokun <odinokun@gmail.com>
2024-03-05 11:17:18 -0500

    Refactor code and remove unused modules and files

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
-$(function() {
-
+$(function () {
   require('./modules/svg4everybody')();
+  require('./modules/swiper')();
 
   // require('./modules/scrollUp')();
-  // require('./modules/noJs')();
   // require('./modules/maskedinput')();
   // require('./modules/scroll2anchor')();
   // require('./modules/popup')();
-
 });
